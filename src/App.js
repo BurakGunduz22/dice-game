5faa71e40a8dc34b6f236706d5395a75f3dca90e
BurakGunduz22 <burakgunduz22@gmail.com>
2023-03-13 15:06:18 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import dice1 from './dice1.png';
+import dice2 from './dice2.png';
+import dice3 from './dice3.png';
+import dice4 from './dice4.png';
+import dice5 from './dice5.png';
+import dice6 from './dice6.png';
+const dices =[dice1,dice2,dice3,dice4,dice5,dice6];
+const style = {
+  padding : 100,
+}
+function DiceRoller() {
+  const [FirstdiceValue, setFirstDiceValue] = useState(1);
+  const [SeconddiceValue, setSecondDiceValue] = useState(1);
+  function rollDice() {
+    const FirstDiceValue = Math.floor(Math.random() * 6) + 1;
+    const SecondDiceValue = Math.floor(Math.random() * 6) + 1;
+    setFirstDiceValue(FirstDiceValue);
+    setSecondDiceValue(SecondDiceValue);
+  }
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+      <h2>Dice Roller</h2>
+      <img src = {dices[FirstdiceValue-1]}/>
+      <img src = {dices[SeconddiceValue-1]}/>
+      <button onClick={rollDice}>Roll Dice</button>
     </div>
   );
 }
 
-export default App;
+export default DiceRoller;
